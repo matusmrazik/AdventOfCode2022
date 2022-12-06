d08dc81f3a6bdc619b392045b7bfe15fe6f25f6b
Matúš Mrázik <matus.mrazik2@gmail.com>
2022-12-06 10:38:22 +0100

    Day 1 and 2

---
+++ AOC22_Python/day01.py
@@ -0,0 +1,34 @@
+import os
+
+
+class Day01:
+    INPUT_FILE = os.path.join(os.path.dirname(__file__), '../Inputs', 'day01.txt')
+    TEST_FILE = os.path.join(os.path.dirname(__file__), '../Inputs', 'test01.txt')
+
+    def __init__(self, test=False):
+        input_path = self.TEST_FILE if test else self.INPUT_FILE
+        self.inputs = []
+        with open(input_path, 'r') as infile:
+            calories = []
+            for line in infile:
+                stripped = line.strip()
+                if len(stripped) == 0:
+                    if calories:
+                        self.inputs.append(calories)
+                        calories = []
+                    continue
+                calories.append(int(stripped))
+            if calories:
+                self.inputs.append(calories)
+
+    def solve1(self):
+        return max([sum(_) for _ in self.inputs])
+
+    def solve2(self):
+        return sum(sorted([sum(_) for _ in self.inputs], reverse=True)[:3])
+
+
+if __name__ == '__main__':
+    x = Day01()
+    print(x.solve1())
+    print(x.solve2())
---
+++ AOC22_Python/day02.py
@@ -0,0 +1,37 @@
+import os
+
+SCORES_1 = {
+    'A': {'X': 4, 'Y': 8, 'Z': 3},
+    'B': {'X': 1, 'Y': 5, 'Z': 9},
+    'C': {'X': 7, 'Y': 2, 'Z': 6},
+}
+
+SCORES_2 = {
+    'A': {'X': 3, 'Y': 4, 'Z': 8},
+    'B': {'X': 1, 'Y': 5, 'Z': 9},
+    'C': {'X': 2, 'Y': 6, 'Z': 7},
+}
+
+
+class Day02:
+    INPUT_FILE = os.path.join(os.path.dirname(__file__), '../Inputs', 'day02.txt')
+    TEST_FILE = os.path.join(os.path.dirname(__file__), '../Inputs', 'test02.txt')
+
+    def __init__(self, test=False):
+        input_path = self.TEST_FILE if test else self.INPUT_FILE
+        self.inputs = []
+        with open(input_path) as infile:
+            for line in infile:
+                self.inputs.append((line[0], line[2]))
+
+    def solve1(self):
+        return sum(SCORES_1[a][b] for a, b in self.inputs)
+
+    def solve2(self):
+        return sum(SCORES_2[a][b] for a, b in self.inputs)
+
+
+if __name__ == '__main__':
+    x = Day02()
+    print(x.solve1())
+    print(x.solve2())
